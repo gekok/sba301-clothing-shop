918f496464af996a0413001499b08c381a708a04
baonv <baonv.gog@gmail.com>
2026-05-31 23:21:41 +0700

    add review ui demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import Home from './pages/customer/Home.jsx';
 import Dashboard from './pages/admin/Dashboard.jsx';
 import POS from './pages/staff/POS.jsx';
 
+import OrderListDemo from './review-demo/OrderListDemo.jsx';
+import OrderDetailDemo from './review-demo/OrderDetailDemo.jsx';
+import ReviewSuccessDemo from './review-demo/ReviewSuccessDemo.jsx';
+import ProductDetailDemo from './review-demo/ProductDetailDemo.jsx';
+
 function App() {
   return (
     <>
@@ -17,6 +22,7 @@ function App() {
               <Nav.Link as={Link} to="/">Trang chủ</Nav.Link>
               <Nav.Link as={Link} to="/admin">Admin</Nav.Link>
               <Nav.Link as={Link} to="/staff/pos">Staff POS</Nav.Link>
+              <Nav.Link as={Link} to="/demo" className="text-warning fw-medium">Đánh giá (Demo)</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -24,9 +30,15 @@ function App() {
 
       <Container className="py-4">
         <Routes>
+          {/* Các trang gốc của dự án */}
           <Route path="/" element={<Home />} />
           <Route path="/admin" element={<Dashboard />} />
           <Route path="/staff/pos" element={<POS />} />
+
+          <Route path="/demo" element={<OrderListDemo />} />
+          <Route path="/demo-order/:orderCode" element={<OrderDetailDemo />} />
+          <Route path="/demo-order/:orderCode/success" element={<ReviewSuccessDemo />} />
+          <Route path="/demo-product/:productId" element={<ProductDetailDemo />} />
         </Routes>
       </Container>
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import App from './App.jsx';
+import { ReviewProviderDemo } from './review-demo/ReviewContextDemo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ReviewProviderDemo>
+        <App />
+      </ReviewProviderDemo>
     </BrowserRouter>
   </React.StrictMode>,
 );
