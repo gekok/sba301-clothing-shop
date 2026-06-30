274633a73e2762a0485733d8edc987aeabb43221
baonv <baonv.gog@gmail.com>
2026-07-01 01:58:05 +0700

    add review backend

---
+++ backend/src/main/java/com/sba301/ecommerce/features/review/dto/ReviewResponse.java
@@ -0,0 +1,21 @@
+package com.sba301.ecommerce.features.review.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewResponse {
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private String authorName;
+    private Long orderItemId;
+    private Integer rating;
+    private String comment;
+    private String createdAt;
+}
